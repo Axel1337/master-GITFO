692ace3b217602f66f3d2bd25c9d9ccc925c21b8
Axel <axelroempke@hotmail.com>
2019-10-25 13:24:53 +0200

    La till en sats som gör att avnändaren kan anropa egna värden till constructorn med en scanner. Och även att man bara kan använda Integer

--- PlayGuesser.java
+++ PlayGuesser.java
@@ -1,10 +1,45 @@
 package GitInl;
 
+import java.util.Scanner;
+
 public class PlayGuesser {
 
 	public static void main(String[] args) {
 		
-		Guesser guesser = new Guesser(0, 1000);
+		
+		Scanner sc = new Scanner(System.in);
+		boolean kollaInt = false;
+		int l = 0;
+		int h = 0;
+
+		System.out.println("Welcome to the guessing game, choose a Low, and a High");
+		System.out.print("Low: ");
+
+		do {
+			if (sc.hasNextInt() == true) {
+				l = sc.nextInt();
+				break;
+			} else {
+				System.out.print("Please enter an integer value: ");
+				sc.next();
+			}
+		} while (kollaInt == false);
+
+		System.out.print("High: ");
+
+		do {
+			if (sc.hasNextInt() == true) {
+				h = sc.nextInt();
+				break;
+			} else {
+				System.out.print("Please enter an integer value: ");
+				sc.next();
+			}
+		} while (kollaInt == false);
+
+		
+		
+		Guesser guesser = new Guesser(l, h);
 		guesser.start();
 
 	}
